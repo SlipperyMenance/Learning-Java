2e9611423adbeb4753587dd89144d8be89d3c0ee
SlipperyMenance <91383170+SlipperyMenance@users.noreply.github.com>
2025-07-27 16:21:25 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,64 @@
 public class Main {
     public static void main(String[] args) {
 
-        ///Вывести длину отрезка, проходящей через две точки
+///Вывести последнюю цифру числа
+
+        int a, b, c;
+        a = (int) (Math.random() * (10 - 1) + 1);
+        b = (int) (Math.random() * (10 - 1) + 1);
+        c = (int) (Math.random() * (10 - 1) + 1);
+
+        System.out.println("Число = " + a + b + c + "; a = " + a + "; b = " + b + "; c = " + c);
+        System.out.println("Третья цифра = " + c);
+
+            int x = 354;
+            System.out.println(x - 350);
+
+
+                int d = 347;
+                System.out.println(d % 10);
+
+///Найти сумму цифр ab + cd числа abcd
+
+        int a1, b1, c1, d1, ab, cd;
+        a1 = (int) (Math.random() * (10 - 1) + 1);
+        b1 = (int) (Math.random() * (10 - 1) + 1);
+        c1 = (int) (Math.random() * (10 - 1) + 1);
+        d1 = (int) (Math.random() * (10 - 1) + 1);
+
+        System.out.println("Number abcd = " + a1 + b1 + c1 + d1 + ";");
+        System.out.println("ab  = " + a1 + b1 + "; cd= " + c1 + d1 + ";");
+        ab = a1 * 10 + b1;
+        cd = c1 * 10 + d1;
+        int sum = ab + cd;
+        System.out.println("Sum ab + cd = " + sum);
+
+
+        int abcd;
+        abcd = (int) (Math.random() * 9000) + 1000;
+
+        System.out.println("Number abcd = " + abcd + ";");
+        int f = abcd / 100;
+        int s = abcd % 100;
+        int res1 = f + s;
+        System.out.println("Num ab=" + f + "; num bc=" + s + "; sum ab+cd=" + res1);
+
+///Вывести 3-ю цифру 5ти-значного числа
+
+        int a2, b2, c2, d2, e2;
+        a2 = (int) (Math.random() * (10 - 1) + 1);
+        b2 = (int) (Math.random() * (10 - 1) + 1);
+        c2 = (int) (Math.random() * (10 - 1) + 1);
+        d2 = (int) (Math.random() * (10 - 1) + 1);
+        e2 = (int) (Math.random() * (10 - 1) + 1);
+
+        int num = a2 * 10000 + b2 * 1000 + c2 * 100 + d2 * 10 + e2;
+        System.out.println("Number abcde=" + num + ";");
+        int cal = num / 100;
+        int res2 = cal % 10;
+        System.out.println("3-rd number=" + res2);
+
+///Вывести длину отрезка, проходящей через две точки
 
         int x1, y1, x2, y2;
         x1 = (int) (Math.random() * (10 - 1) + 1);
