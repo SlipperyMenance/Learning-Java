8cc4a9af324ca6a9cd50abc2667870ca5c991fd1
SlipperyMenance <91383170+SlipperyMenance@users.noreply.github.com>
2025-07-27 16:00:45 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        ///Вывести длину отрезка, проходящей через две точки
+        ///Вывести 3-ю цифру 5ти-значного числа
 
-        int x1, y1, x2, y2;
-        x1 = (int) (Math.random() * (10 - 1) + 1);
-        y1 = (int) (Math.random() * (10 - 1) + 1);
-        x2 = (int) (Math.random() * (10 - 1) + 1);
-        y2 = (int) (Math.random() * (10 - 1) + 1);
+        int a, b, c, d, e;
+        a = (int) (Math.random() * (10 - 1) + 1);
+        b = (int) (Math.random() * (10 - 1) + 1);
+        c = (int) (Math.random() * (10 - 1) + 1);
+        d = (int) (Math.random() * (10 - 1) + 1);
+        e = (int) (Math.random() * (10 - 1) + 1);
 
-        System.out.println("Dot1 (" + x1 + ";" + y1 + ");");
-        System.out.println("Dot2 (" + x2 + ";" + y2 + ");");
-
-        //Длина отрезка AB = √[(x₂ - x₁)² + (y₂ - y₁)²]
-
-        double lenght = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        System.out.println("Lengt AB=" + lenght);
+        int num = a * 10000 + b * 1000 + c * 100 + d * 10 + e;
+        System.out.println("Number abcde=" + num + ";");
+        int cal = num / 100;
+        int res = cal % 10;
+        System.out.println("3-rd number=" + res);
     }
 }
